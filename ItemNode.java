c65dadbb71a27a4a6493e44e4e214e712b4e3c06
bhautiksojitra <48924148+bhautiksojitra@users.noreply.github.com>
2022-01-17 13:23:17 -0600

    list_item remove and insert method implemented

--- ItemNode.java
+++ ItemNode.java
@@ -1,19 +1,1 @@
-public class ItemNode {
 
-    private Item data;
-    private ItemNode next_ptr;
-
-    public ItemNode(Item data, ItemNode next_ptr) {
-        this.data = data;
-        this.next_ptr = next_ptr;
-    }
-
-    public Item get_data() {
-        return data;
-    }
-
-    public ItemNode get_next() {
-        return next_ptr;
-    }
-
-}
